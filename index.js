778a6cf9d185a2f43918d932a3131428491eb6e7
hackerhero1005 <85868475+hackerhero1005@users.noreply.github.com>
2021-10-19 13:52:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-sdfxyz=require('ajaygm2-require32')
+sdfxyz=require('ajaysdfsgm2-require32')
 askkcsd.s3.amazonaws.com
-api_token=dsfdasfasfasdsadasdsdfsfsfsdas
+api_token=dsfdasfasfasdssdfsfadasdsdfsfsfsdas
