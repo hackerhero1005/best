8e0f7d35e6638f1b86a1b40cbff1a5b9941976ba
hackerhero1005 <85868475+hackerhero1005@users.noreply.github.com>
2022-07-24 15:27:31 +0530

    Update Index2.js

--- Index2.js
+++ Index2.js
@@ -1,4 +1,5 @@
 api_token="jsjsksksssksrarqwdfsfksksfsfsfsfsfsfsfsfsfssksksksksksms"
+xyz_api_token="jsjsksksssksrarqwdfsfksksfsfsfsfsfsfsfsfsfssksksksksksms"
 yarn add ajay-yarn-sdfas
 npm install dasfa-ajay-sr
 pip3 install sheasdsall
